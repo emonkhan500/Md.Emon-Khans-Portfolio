2459bc1d0a79910eed7f948ce1c76a52e81965bc
emonkhan500 <khdilo47@gmail.com>
2024-11-13 00:00:17 +0600

    little change"

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 <h1 className='mt-5 text-4xl text-white'>Who am i?</h1>
                 <p className='mt-4 text-gray-400 text-lg'>I am a junior front-end developer. Strong passion, love and self-improvement for technology and coding so far Frontend has learned - ReactJS, JavaScript, Tailwind CSS, Bootstrap, HTML, CSS also mobile responsive, all device responsive and various packages, frameworks, libraries. Also learned and learning for backend - MongoDB, NodeJS, Express.js, JWT, and Firebase.
 
-Besides practice and work, I am constantly learning new things and love learning new technologies. I like taking on new challenges. My next target is to become a very good full-stack developer.
+Besides practice and work,  I am constantly learning new things and love learning new technologies. I like taking on new challenges. My next target is to become a very good full-stack developer.
 
 </p>
             </div>
